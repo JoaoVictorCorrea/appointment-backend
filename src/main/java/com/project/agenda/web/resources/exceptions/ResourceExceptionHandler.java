9e032cfb2655dd585c6b01bacb63123ee2b674b0
João Victor Correa <correa.joaov25@gmail.com>
2024-07-25 10:37:15 -0300

    :bug: fix: Add DataIntegrityViolationException in ResourceExceptionHandler

--- src/main/java/com/project/agenda/web/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/project/agenda/web/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.project.agenda.web.resources.exceptions;
 import java.time.Instant;
 import java.time.format.DateTimeParseException;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -139,6 +140,23 @@ public class ResourceExceptionHandler {
 
         error.setError("Parameter Exception");
         error.setMessage(exception.getMessage());
+        error.setPath(request.getRequestURI());
+        error.setStatus(status.value());
+        error.setTimeStamp(Instant.now());
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException exception,
+            HttpServletRequest request) {
+
+        StandardError error = new StandardError();
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        error.setError("Database Integrity Exception");
+        error.setMessage("Conflito com a integração dos dados.");
         error.setPath(request.getRequestURI());
         error.setStatus(status.value());
         error.setTimeStamp(Instant.now());
