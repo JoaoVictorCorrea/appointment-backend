964248383746b6a392662d2b527cdee9d8ddafb8
João Victor Correa <correa.joaov25@gmail.com>
2024-11-23 13:06:40 -0300

    :sparkles: feat: dev and prod profiles with PostgreSQL

--- src/main/java/com/project/agenda/domain/entities/testrunner/BDRunnerTestEntities.java
+++ src/main/java/com/project/agenda/domain/entities/testrunner/BDRunnerTestEntities.java
@@ -48,31 +48,34 @@ public class BDRunnerTestEntities implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         
-        Client c1 = clientRepository.findById(1L).get();
-        System.out.println(c1);
-
-        Professional p1 = professionalRepository.findById(4L).get();
-        System.out.println(p1);
-
-        Area a1 = areaRepository.findById(3).get();
-        System.out.println(a1);
-
-        AppointmentType apt1 = appointmentTypeRepository.findById(1).get();
-        System.out.println(apt1);
+//        Client c1 = clientRepository.findById(1L).get();
+//        System.out.println(c1);
+//
+//        Professional p1 = professionalRepository.findById(4L).get();
+//        System.out.println(p1);
+//
+//        Area a1 = areaRepository.findById(3).get();
+//        System.out.println(a1);
+//
+//        AppointmentType apt1 = appointmentTypeRepository.findById(1).get();
+//        System.out.println(apt1);
+//
+//        WorkScheduleItem ws = new WorkScheduleItem(DayOfWeek.MONDAY, LocalTime.parse("08:00:00"),LocalTime.parse("12:00:00"), 8, 30);
+//        this.workScheduleItemRepository.save(ws);
+//
+//        Appointment appointment = new Appointment();
+//        appointment.setClient(c1);
+//        appointment.setProfessional(p1);
+//        appointment.setArea(a1);
+//        appointment.setAppointmentType(apt1);
+//        appointment.setDate(LocalDate.of(2024, 06, 17));
+//        appointment.setStartTime(LocalTime.of(10, 00));
+//        appointment.setEndTime(LocalTime.of(10, 30));
+//        appointment.setComments("Urgente!");
+//
+//        appointmentRepository.save(appointment);
 
         WorkScheduleItem ws = new WorkScheduleItem(DayOfWeek.MONDAY, LocalTime.parse("08:00:00"),LocalTime.parse("12:00:00"), 8, 30);
         this.workScheduleItemRepository.save(ws);
-        
-        Appointment appointment = new Appointment();
-        appointment.setClient(c1);
-        appointment.setProfessional(p1);
-        appointment.setArea(a1);
-        appointment.setAppointmentType(apt1);
-        appointment.setDate(LocalDate.of(2024, 06, 17));
-        appointment.setStartTime(LocalTime.of(10, 00));
-        appointment.setEndTime(LocalTime.of(10, 30));
-        appointment.setComments("Urgente!");
-
-        appointmentRepository.save(appointment);
     }
 }
